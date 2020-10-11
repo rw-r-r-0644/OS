ef3cba6de060ff13b67c18df78417bac3954c7a3
rw-r-r-0644 <r.r.qwertyuiop.r.r@gmail.com>
2020-10-11 17:31:03 +0200

    rename kernel start/end symbols

--- kern/inc/mem/kernel.h
+++ kern/inc/mem/kernel.h
@@ -6,8 +6,8 @@
 #pragma once
 #include <stdint.h>
 
-extern uint32_t p_kernel_start;
-extern uint32_t p_kernel_end;
+extern uint32_t __kernel_start;
+extern uint32_t __kernel_end;
 
-#define kernel_start		((uintptr_t)&p_kernel_start)
-#define kernel_end			((uintptr_t)&p_kernel_end)
+#define kernel_start		((uintptr_t)&__kernel_start)
+#define kernel_end			((uintptr_t)&__kernel_end)
